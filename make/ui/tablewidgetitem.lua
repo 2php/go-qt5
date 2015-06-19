96daef289a3019cc47d1f8f52fe612581c13cc34
Matthew Iannucci <miannucci@navatekltd.com>
2015-06-19 10:23:18 -0400

    First pass at tablewidgetitem

--- make/ui/tablewidgetitem.lua
+++ make/ui/tablewidgetitem.lua
@@ -8,6 +8,35 @@ funcs = [[
 + InitWithText(text string)
 - Close()
 
+SetBackground(b Brush)
+Background()(b Brush)
+SetCheckState(c CheckState)
+CheckState()(c CheckState)
+Column()(i int)
+SetFlags(f ItemFlags)
+Flags()(f ItemFlags)
+SetFont(f Font)
+Font()(f Font)
+SetForeground(b Brush)
+Foreground()(b Brush)
+SetIcon(i Icon)
+Icon()(i Icon)
+SetSelected(b bool)
+IsSelected()(b bool)
+Row()(i int)
+SetSizeHint(s Size)
+SizeHint()(s Size)
+SetStatusTip(s string)
+StatusTip()(s string)
+SetText(s string)
+Text()(s string)
+SetTextAlignment(i int)
+TextAlignment()(i int)
+SetToolTip(s string)
+ToolTip()(s string)
+SetWhatsThis(s string)
+WhatsThis()(s string)
+
 ]]
 
 qtdrv = {
@@ -23,5 +52,34 @@ drvNewObj(a0, new ShellTableWidgetItem(drvGetString(a1)));
 Close = [[
 drvDelObj(a0,self);
 ]],
+
+SetBackground = "setBackground",
+Background = "background",
+SetCheckState = "setCheckState",
+CheckState = "checkState",
+Column = "column",
+SetFlags = "setFlags",
+Flags = "flags",
+SetFont = "setFont",
+Font = "font",
+SetForeground = "setForeground",
+Foreground = "foreground",
+SetIcon = "setIcon",
+Icon = "icon",
+SetSelected = "setSelected",
+IsSelected = "isSelected",
+Row = "row",
+SetSizeHint = "setSizeHint",
+SizeHint = "sizeHint",
+SetStatusTip = "setStatusTip",
+StatusTip = "statusTip",
+SetText = "setText",
+Text = "text",
+SetTextAlignment = "setTextAlignment",
+TextAlignment = "textAlignment",
+SetToolTip = "setToolTip",
+ToolTip = "toolTip",
+SetWhatsThis = "setWhatsThis",
+WhatsThis = "whatsThis",
 
 }
