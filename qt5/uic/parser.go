7eabdc3fa5a204021988472433a74b7acacf6b69
Matthew Iannucci <mpiannucci@gmail.com>
2015-11-15 15:55:17 -0500

    Decide on parser functionality

--- qt5/uic/parser.go
+++ qt5/uic/parser.go
@@ -1,1 +1,21 @@
 package main
+
+type Parser struct {
+	DesignFile *UiFile
+}
+
+func (p *Parser) readUiFile(filename string) {
+	uiFile, _ := extractUiFileData(filename)
+	p.DesignFile = uiFile
+}
+
+func NewParser() *Parser {
+	parser := &Parser{}
+	return parser
+}
+
+func NewParserFromUiFile(filename string) *Parser {
+	uiFile, _ := extractUiFileData(filename)
+	parser := &Parser{uiFile}
+	return parser
+}
