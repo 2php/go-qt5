b754dbdbfe02e628d5002ff8d83b922f54415707
mutse <yyhoo2.young@gmail.com>
2016-07-21 07:31:42 +0800

    update lcdnumber.go

--- examples/lcdnumber.go
+++ examples/lcdnumber.go
@@ -8,7 +8,7 @@ func main() {
     qt5.Main(func() {
         lcd := qt5.NewLCDNumber()
         lcd.SetWindowTitle("LCDNumber")
-	lcd.SetSizev(200, 50)
+        lcd.SetSizev(200, 50)
         lcd.SetSegmentStyle(1)
         lcd.DisplayWithString("2016")
         defer lcd.Close()
